82e263f659105543ebf5059451973d5ea8472a40
root <root@Laptop>
2024-08-24 21:45:47 +0100

    add some extra examples

--- abc.py
+++ abc.py
@@ -1,9 +1,6 @@
-string = "Hi! Hello!"
-x = list(string)
-newString = ""
-for char in x:
-    if char != "!":
-        newString += char
-print(newString)
+basket = ["apple", "orange", "cherry","cherry","cherry","cherry","strawberry","cherry","cherry"]
+while "cherry" in basket:
+    basket.remove("cherry")
+print(basket)
 # type: ignore
 
