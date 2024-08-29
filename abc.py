df7d0824163a94b3293add4cf0d476c8e32d6589
root <root@Laptop>
2024-08-29 19:03:10 +0100

    adds chapter 12

--- abc.py
+++ abc.py
@@ -2,5 +2,8 @@ basket = ["apple", "orange", "cherry","cherry","cherry","cherry","strawberry","c
 while "cherry" in basket:
     basket.remove("cherry")
 print(basket)
+
+abc = "easy as 1, 2, 3"
+print(abc.split())
 # type: ignore
 
