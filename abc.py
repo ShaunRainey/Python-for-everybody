c7ab6c83350da1e52a5f2875a4d21f792283edec
ShaunRainey <151593013+ShaunRainey@users.noreply.github.com>
2024-09-19 13:59:35 +0100

    Update abc.py

--- abc.py
+++ abc.py
@@ -1,9 +1,3 @@
-basket = ["apple", "orange", "cherry","cherry","cherry","cherry","strawberry","cherry","cherry"]
-while "cherry" in basket:
-    basket.remove("cherry")
-print(basket)
 
-abc = "easy as 1, 2, 3"
-print(abc.split())
 # type: ignore
 
