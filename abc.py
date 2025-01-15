4d6cd95eff124304e4db6af34e9306af02564ae0
ShaunRainey <151593013+ShaunRainey@users.noreply.github.com>
2025-01-15 13:27:01 +0000

    Update abc.py

--- abc.py
+++ abc.py
@@ -1,3 +1,10 @@
-input("")
-# type: ignore
+num = "444444444444444444444444444444444444"
 
+count = 0
+i = 0
+while i < len(num):
+    if num[i] == "4":
+        count += 1
+        i +=1
+
+print(count)
